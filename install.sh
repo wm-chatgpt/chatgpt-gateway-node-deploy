d3b6f83e8fce0a8f83ff95caf4824f448db5afb4
lihang15 <lihang15@meituan.com>
2024-03-22 13:03:00 +0800

    init

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+## 克隆仓库到本地
+echo "clone repository..."
+git clone -b deploy  --depth=1 https://github.com/hanglegehang/chatgpt-proxy-node-deploy.git chatgpt-proxy-node
+
+## 进入目录
+cd chatgpt-proxy-node
+
+docker compose pull
+docker compose up -d --remove-orphans
+
+## 提示信息
+echo "服务启动成功，请访问 http://ip:8100"
